9c561de90f19c33e489f917bb005343d3b3561e2
osamaraed53 <osamaraed53@gmail.com>
2023-10-05 19:04:16 +0300

    solve for first seven question

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # problemSolving_js
+
+this repo contain simple problem and solve for it in Array topic.
+
+
+Problems
+---------
+1- create a function that takes two arguments, one is an array and the other is a number, then return the index of the given value or return -1 if the element cannot be found.
+
+2- create a function that takes an array of integers between 1 and 100 except for one, and returns the missing number.
+
+3- create a function that takes an array of integers and returns a new array with the same integers without the duplicates 
+
+4- create a function that takes an array of integers and returns the average of all the numbers in it 
+
+5- Write a function that, takes an array of integers as input, iterates over the array, and returns a new array.
+ The returned array should contain the result of raising 2 to the power of the original input element.
+For example,([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and 2 ^ 3 = 8.
+Use for and forEach and then map to solve this question to see the difference between the three ways 
+
+6- Write a function that, takes an array of numbers as input, uses map to return a new array where each element is either the string "even" or the string "odd", based on each value.
+If any element in the array is not a number, the resulting array should have the string "N/A" in its place.
+For example:([1,2,3,"Rawan"]) returns ['odd','even','odd','N/A'].
+
+7- Write a function named fizzbuzz that takes in an array of numbers.
+Iterate over the array using forEach or map to determine the output based on several rules:
+  - If a number is divisible by 3, add the word "Fizz" to the output array.
+  - If the number is divisible by 5, add the word "Buzz" to the output array.
+  - If the number is divisible by both 3 and 5, add the phrase "Fizz Buzz" to the output array.
+  - Otherwise, add the number to the output array.
+Return the resulting output array.
+
+
---
+++ problrm.js
@@ -0,0 +1,116 @@
+ 
+function include(arr , num){
+    for(let i= 0; i <arr.length;i++){
+        if(arr[i]==num){
+            return true;
+        }
+    }
+    return false;
+}
+// q1
+function Q1(arr = [0,1] , num){
+    for(let i =0 ;i <arr.length;i++){
+        if(arr[i]==num){
+            return i ;
+        }
+    }
+    return -1; 
+}
+
+
+// q2
+function Q2(arr){
+    for(let i = 1 ; i<=100;i++){
+        // I'am create include method
+        if(!include(arr,i)){
+            return i;
+        }
+    }
+    return "not missing any thing";
+}
+
+
+//q3
+function Q3(arr){
+    let new_arr = [];
+    for(let i = 0;i <arr.length;i++){
+        // I'am create include method
+        if(!include(new_arr,arr[i])){
+            new_arr.push(arr[i]);
+        }
+    }
+    return new_arr;
+}
+
+// q4
+
+function Q4(arr){
+    let sum =0;
+    let len =arr.length;
+    for(let i=0 ;i<len;i++){
+        sum = sum +arr[i];
+    }
+    let total = sum / len;
+    return total
+
+}
+
+//Q5 
+function Q5_byForEach(arr){
+    let new_arr =[];
+    arr.forEach(element => {
+        new_arr.push(2**element)
+    });
+
+    return new_arr;
+}
+
+function Q5_byMAp(arr){
+    let new_arr =
+    arr.map((element) =>2**element );
+
+    return new_arr;
+}
+
+
+//q6
+
+function Q6(arr){
+
+    let new_arr = arr.map(
+        (element) => {
+            if(isNaN(element)){
+                return "N/A";
+            }
+            else{
+                if(element%2===0){
+                    return  "even";
+                }
+                else{
+                    return  "odd";
+                }
+            }
+        }
+    );
+  return new_arr;
+
+};
+
+
+// Q7
+function fizzbuzz(arr){ 
+    let new_arr = [];
+    arr.forEach((num) =>{
+        if(num%3==0 && num%5 == 0){
+            new_arr.push("Fizz Buzz");
+        }else if(num%3 == 0){
+            new_arr.push("Fizz");
+        }else if(num%5 == 0){
+            new_arr.push("Buzz");
+        }else{
+            new_arr.push(num);
+        }
+    }  
+    );
+    return new_arr;
+}
